f245118fab31e29e93b7391fbc85b985108e495c
xiaz <1252272125@qq.com>
2018-11-22 21:27:08 +0800

    将bgm的.html .css .js合并成一个js文件（方式一）（推荐）

--- index.js
+++ index.js
@@ -1,22 +1,3 @@
 $(document).ready(function(){
-	// $('#bgm').load('/bgm/bgm.html');   //加入bgm
-	$.ajax({
-		url:'/bgm/bgm.html',
-		success:function(result){
-			$('#bgm').html(result);
-		},
-		error:function(){
-			alert('oops，背景音乐加载失败！可刷新重试~');
-		}
-	});
-
-	$.ajax({
-		url:'/header/header.html',
-		success:function(result){
-			$('#navigation-bar').html(result);
-		},
-		error:function(){
-			alert('oops，导航栏加载失败！可刷新重试~');
-		}
-	});
+	$('#bgm').bgm();   //加入bgm
 })
--- postgraduate/postgraduate.js
+++ postgraduate/postgraduate.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
 		url:'/header/header.html',
 		success:function(result){
 			$('#navigation-bar').html(result);
+			var postgraduate = $('#navigation-bar').find('.postgraduate');
+			postgraduate.css({'color':'grey'});
 		},
 		error:function(){
 			alert('oops，导航栏加载失败！可刷新重试~');
