9c8a0e828150d71ef27d988e7ab389dfcfde3749
xiaz <1252272125@qq.com>
2018-11-24 14:35:00 +0800

    不加hover

--- index.js
+++ index.js
@@ -2,62 +2,58 @@ $(document).ready(function(){
 	$('#bgm').bgm({'top':'70%'});   //加入bgm
 	// $('#navigation-bar').header({'top':'10px'});  //加入导航栏
 	// $('#back-to-top').backToTop({'speed':'30'});  //返回顶部
-	var	allCards = {
+	var allCards = {
 			postGraduate : {
 				cardValue: $('.card-nav').find('.postgraduate'),
-				hoverCard: $('.card-nav').find('.first-hover'),
 				timer: ''
 			},
 			recycle : {
 				cardValue: $('.card-nav').find('.recycle'),
-				hoverCard: $('.card-nav').find('.second-hover'),
 				timer: ''
 			},
 			information : {
 				cardValue: $('.card-nav').find('.information'),
-				hoverCard: $('.card-nav').find('.third-hover'),
 				timer: ''
 			},
 			afe : {
 				cardValue: $('.card-nav').find('.afe'),
-				hoverCard: $('.card-nav').find('.fourth-hover'),
 				timer: ''
 			},
 			rent : {
 				cardValue: $('.card-nav').find('.rent'),
-				hoverCard: $('.card-nav').find('.fifth-hover'),
 				timer: ''
 			},
 			aboutus : {
 				cardValue: $('.card-nav').find('.aboutus'),
-				hoverCard: $('.card-nav').find('.sixth-hover'),
 				timer: ''
 			}
 	},
-		firstBlock = $('.card-nav').find('.first-block'),
-		cardWidth = firstBlock.width(),
-		cardHeight = firstBlock.height();
-	$('.card-nav').css({'line-height':cardHeight / 6 + 'px','font-size':cardWidth / 4 + 'px'});
-	$('.card-nav>div *').css({'border-radius':cardWidth / 2});  //所有卡片设置圆角
+		cardWidth = allCards.postGraduate.cardValue.width(),
+		cardHeight = allCards.postGraduate.cardValue.height()
+	$('.card-nav a').css('line-height',cardHeight / 6 + 'px');
+	$('.card-nav').css('font-size',cardWidth / 4 + 'px');
 	$(window).resize(function(){
-		cardWidth = firstBlock.width();
-		cardHeight = firstBlock.height();
-		$('.card-nav').css({'line-height':cardHeight / 6 + 'px','font-size':cardWidth / 4 + 'px'});
-		$('.card-nav>div *').css({'border-radius':cardWidth / 2});  //所有卡片设置圆角
+		cardWidth = allCards.postGraduate.cardValue.width();
+		$('.card-nav a').css('line-height',cardHeight / 6 + 'px');
+		$('.card-nav').css('font-size',cardWidth / 4 + 'px');
+		for(var prop in allCards) {
+			(function(thisCard) {
+				thisCard.cardValue.css('border-radius',cardWidth / 2);  //所有卡片设置圆角
+			}(allCards[prop]));
+		}
 	})
 
 	for(var prop in allCards) {
 		(function(thisCard){
-			thisCard.hoverCard.hover(
+			thisCard.cardValue.css('border-radius',cardWidth / 2);  //所有卡片设置圆角
+			thisCard.cardValue.hover(
 				function() {
-					console.log('enter');
 					clearTimeout(thisCard.timer);
 					if(!thisCard.cardValue.hasClass('flip')) {
 						thisCard.cardValue.addClass('flip');
 					}
 				},
 				function(){
-					console.log('leave');
 					thisCard.timer = setTimeout(function(){
 						thisCard.cardValue.removeClass('flip');
 					},1000);
